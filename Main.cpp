bfc6b31cf7d82bc0c2c362c0711421cb3b89e91b
warusurayooteru <warusura.yooteru@gmail.com>
2024-09-26 15:44:15 +0900

    条件式にbool型の関数を使用

---
+++ Main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+bool IsDigit(char ch)
+{
+	return '0' <= ch && ch <= '9';
+}
+
+int main()
+{
+	while (true)
+	{
+		char ch;
+		cout << "何か文字を入力して下さい > " << flush;
+		cin >> ch;
+		if (ch == 'Q' || ch == 'q')
+		{
+			break;
+		}
+
+		if (IsDigit(ch))
+		{
+			cout << "数字です。" << endl;
+		}
+		else
+		{
+			cout << "数字ではありません。" << endl;
+		}
+	}
+
+	cout << "終了します。" << endl;
+
+}
+
